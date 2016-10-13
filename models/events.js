080256c7e0be95d513026f1642c3b574fd7807c6
achoisy <alexandre.choisy@gmail.com>
2016-10-13 08:58:41 -0400

    Update Code

--- models/events.js
+++ models/events.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  senderid: { type: String, required: true, unique: true },
+  senderid: { type: String, required: true },
   admin_list: [String],
   password: String,
   event_info: {
@@ -12,8 +12,8 @@ const eventSchema = new Schema({
     start_date: Date,
     end_date: Date,
     location: {
-      latitude: Number,
-      longitude: Number,
+      lat: Number,
+      long: Number,
     },
   },
   welcome_msg: {
