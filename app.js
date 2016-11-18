8811f5ed863b24279ce631453b12c8e47773b4cf
achoisy <alexandre.choisy@gmail.com>
2016-11-18 13:54:37 -0400

    Routes tests

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const TransloaditClient = require('transloadit');
 const msg = require('./lib/message.js');
 const eventData = require('./lib/eventdata.js');
 const mobile = require('./lib/mobile.js');
+const fbsettings = require('./lib/threadSettings.js');
+const attach = require('./lib/attach.js');
+const message = require('./lib/message.js');
 
 // Model for mongoose schema
 const User = require('./models/users');
@@ -639,7 +642,79 @@ messenger.on('message', (message) => {
 });
 
 // Routes
-require('./lib/routes')(app);
+// require('./lib/routes')(app);
+
+// Server API
+app.get('/messenger/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.get('/messenger/init', (req, res) => {
+  fbsettings.add();
+  res.sendStatus(200);
+});
+
+app.get('/messenger/removethread', (req, res) => {
+  fbsettings.remove();
+  res.sendStatus(200);
+});
+
+// Check connection
+app.get('/messenger/checkserver', (req, res) => {
+  console.log('Server connexion OK !');
+  res.sendStatus(200);
+});
+
+// Facebook Webhook
+app.post('/messenger/webhook', (req, res) => {
+  res.sendStatus(200);
+  messenger.handle(req.body);
+});
+
+app.post('/messenger/uploads', (req, res) => {
+  // res.sendStatus(200);
+  const transloaditResponse = JSON.parse(req.body.transloadit);
+  // console.log('transloadit: ', transloadit.fields);
+
+  // Ajoute l'image dans la base attach
+  attach.addNew(transloaditResponse, (attachObject) => {
+    // Affiche la page recap des image uploadées
+    res.render('upload', {
+      senderid: attachObject.userid,
+      eventid: attachObject.eventid,
+      senderImage: [
+        {
+          thumbnail_url: attachObject.thumbnail_url,
+          message: attachObject.message,
+        },
+      ],
+    });
+  });
+});
+
+app.get('/messenger/camera/:senderid/:eventid/', (req, res) => {
+  res.render('camera', {
+    eventid: req.params.eventid,
+    senderid: req.params.senderid,
+  });
+});
+
+app.get('/messenger/gallery/:eventid/', (req, res) => {
+  attach.listByEventId(req.params.eventid, (attachArray) => {
+    if (attachArray === false) {
+      message.send('Aucune photo dans la gallerie, désolé...');
+    } else {
+      res.render('gallery', {
+        eventImage: attachArray,
+      });
+    }
+  });
+});
 
 //------------------------------------------------------------------------------------
 // Start Server
