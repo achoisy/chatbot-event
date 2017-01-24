7594e3a3aff8ef599989721e4b625430636b7b4b
achoisy <alexandre.choisy@gmail.com>
2017-01-24 16:32:33 -0400

    Update on database model and controler
    Add user profil to attach and prints

--- models/attach.js
+++ models/attach.js
@@ -12,6 +12,15 @@ const attachSchema = new Schema({
   optimiseImageUrl: String,
   thumbnail_url: { type: String },
   author_pic: String, // cloudinary public Id
+  user_profile: {
+    first_name: String,
+    last_name: String,
+    profile_pic: String,
+    profile_public_id: String,
+    locale: String,
+    timezone: String,
+    gender: String,
+  },
   message: { type: String, maxlength: 120 },
   public_id: { type: String },
   version: Number,
